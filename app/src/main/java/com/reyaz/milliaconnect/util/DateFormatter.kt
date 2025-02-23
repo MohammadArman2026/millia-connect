6a74b1a8ce8611615f17e1ba044cc8f6b2d62cc8
MD SADIQUE <118601913+sadiquereyaz@users.noreply.github.com>
2025-02-23 23:34:47 +0530

    Create DateFormatter.kt

---
+++ app/src/main/java/com/reyaz/milliaconnect/util/DateFormatter.kt
@@ -0,0 +1,10 @@
+// inputFormat = "dd/MM/yyyy"
+// outputFormat = "dd MMM yyyy"
+fun String.formatDate(inputFormat : String, outputFormat : String) : String {
+    return try {
+        val date = SimpleDateFormat(inputFormat).parse(this)    // parse input
+        SimpleDateFormat(outputFormat).format(date)    // format output
+    } catch (e: ParseException) {
+        ""
+    }
+}
